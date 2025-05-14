e587b475f8ea42c8056d7a29c36a46e4bbea53bb
Ramdan <dadanisme@upi.edu>
2025-05-14 11:04:57 +0800

    Initial Commit

---
+++ ADA-OpenHouse/ADA_OpenHouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADA_OpenHouseApp.swift
+//  ADA-OpenHouse
+//
+//  Created by Ramdan on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct ADA_OpenHouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
