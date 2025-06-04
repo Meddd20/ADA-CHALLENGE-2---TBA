2304c4077eeb3afb6aef116f62392339d8ad14c9
Medhiko Biraja <mbirajabiraja@gmail.com>
2025-06-04 15:26:48 +0800

    fix bug

---
+++ ADA-OpenHouse/Helpers/ColorManager.swift
@@ -0,0 +1,12 @@
+//
+//  ColorManger.swift
+//  ADA-OpenHouse
+//
+//  Created by Medhiko Biraja on 03/06/25.
+//
+
+import SwiftUI
+
+class ColorManger: ObservableObject {
+    @Published var primaryColor: Color = .indigo
+}
